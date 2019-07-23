1e3f323126d8e7b10330eeda73f94e57060fffe8
Josh Sura <joshua.p.sura@verizon.com>
2019-07-23 13:58:06 -0400

    move files and create directory code

---
+++ copycat01.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+##The following will create a DIR and add files that do not already exist
+import shutil
+import os
+os.chdir("/home/student/mycode/")
+shutil.copy("5g_research/snd_network.txt", "5g_research/snd_network.txt.copy")
+shutil.copytree("5g_research/", "5g_research_backup/")
